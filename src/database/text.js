b9744c7cfae76c268c18106e86b80e2598931d19
ederdaniel22 <60990935+ederdaniel22@users.noreply.github.com>
2020-08-13 22:55:24 -0400

    Complementação db
    Complementamos o db.js e testamos as rotas da aplicação, inccluimos os mecanismos de busca para complementar as informações do banco de dados.

--- src/database/text.js
+++ src/database/text.js
@@ -12,7 +12,7 @@ Database.then(async(db) => {
     }
 
     classValue = {
-        subject: "Química",
+        subject: 1,
         cost: "20",
         //proffy id virá pelo banco de dados
     }
@@ -38,10 +38,31 @@ Database.then(async(db) => {
     //todos os proffys
 
     const selectedProffys = await db.all("SELECT * FROM proffys")
-    console.log(selectedProffys)
+        //console.log(selectedProffys)
 
     //consultar as classes de um determinado professor
     // e trazer junto os dados do professor
 
+    const selectClassesAndProffys = await db.all(`
+    SELECT classes.*, proffys.* 
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE classes.proffy_id = 1;
+    `)
+        //console.log(selectClassesAndProffys)
 
+    //o horário que a pesoa trabalha, por exemplo é das 8h - 18h
+    //o horário do time_from (8h) precisa ser menor ou igual ao horário solicitado
+    //o time_to precisa ser acima
+
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "520"
+        AND class_schedule.time_to > "520"
+    `)
+
+    //console.log(selectClassesSchedules)
 })
